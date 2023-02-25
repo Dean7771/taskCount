fae370d0cf4452b18144163ca2b354c5afb5beb6
Daniil Yak <swat27russ@mail.ru>
2023-02-25 11:03:33 +0300

    minfix

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
 function submit() {
-  var a = 5;
+  let inputOrg = document.getElementById("1").value;
+  let inputBase = document.getElementById("2").value;
+  let inputLevel = document.getElementById("3").value;
+  let inputText = document.getElementById("4").value;
 
-  alert(a);
+  if ((inputLevel = 1)) inputLevel_1 = "Легко";
+  else if ((inputLevel = 2)) inputLevel_1 = "Средне";
+  else if ((inputLevel = 2)) inputLevel_1 = "Срочно";
+
+  var con = confirm(
+    "Организация: " +
+      inputOrg +
+      "\nНазвание базы данных: " +
+      inputBase +
+      "\nПриоритет задачи: " +
+      inputLevel_1 +
+      "\nОписание задачи: " +
+      inputText
+  );
+
+  if ((con = false)) {
+    alert("Мы отправили все данные.");
+  } else {
+    alert("Заполните повторно, если допустили ошибку");
+  }
+
+  /*   console.log(inputOrg);
+  console.log(inputBase);
+  console.log(inputLevel_1);
+  console.log(inputText); */
 }
